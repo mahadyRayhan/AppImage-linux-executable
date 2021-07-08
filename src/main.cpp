8148b5304e0fa19f72d01723482cfb2fb8a927bd
mahady hasan rayhan <rayhan.feluda@gmail.com>
2021-07-08 22:54:02 +0900

    QTbranch
    QTbranch

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QFileInfo>
+#include <QIcon>
+#include "easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP
 using namespace std;
 
-int main() {
-    cout << "Hello World!"<<endl;
+
+int main(int argc, char *argv[])
+{
+
+    el::Loggers::addFlag(el::LoggingFlag::StrictLogFileSizeCheck);  
+  
+    el::Configurations conf("my_log.conf");  
+  
+         /// The configuration of a certain logger can be set separately  
+    el::Loggers::reconfigureLogger("default", conf);  
+  
+         /// You can also set all logger configurations  
+    el::Loggers::reconfigureAllLoggers(conf);
+
+    QGuiApplication app(argc, argv);
+    QFileInfo fi(app.applicationDirPath() + "/../share/pixmaps/qtquickapp.png");
+    QGuiApplication::setWindowIcon(QIcon(fi.absoluteFilePath()));
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    LOG(INFO) << "QT Works";
+    app.exec();
+
     return 0;
+    
 }
